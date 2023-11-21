65edf833b3526f27690ffa57729ea1bd5145db80
gzaros <templatewebvo@gmail.com>
2023-11-21 02:27:59 -0800

    AirBnB_clone_v2

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -1,15 +1,14 @@
 #!/usr/bin/python3
-"""This module defines a class to manage file storage for hbnb clone"""
+"""class to manage fileStorage for hbnb clone"""
 import json
 
-
 class FileStorage:
-    """This class manages storage of hbnb models in JSON format"""
+    """class to managestorage of hbnb models in format JSON"""
     __file_path = 'file.json'
     __objects = {}
 
     def all(self, cls=None):
-        """Returns a dictionary of models currently in storage"""
+        """get dictionary of models currently in fileStorage"""
         if cls is None:
             return FileStorage.__objects
         else:
@@ -20,11 +19,11 @@ class FileStorage:
             return class_objects
 
     def new(self, obj):
-        """Adds new object to storage dictionary"""
+        """add new Objet to fileStorage dictionary"""
         self.all().update({obj.to_dict()['__class__'] + '.' + obj.id: obj})
 
     def save(self):
-        """Saves storage dictionary to file"""
+        """save fileStorage dictionary to file"""
         with open(FileStorage.__file_path, 'w') as f:
             temp = {}
             temp.update(FileStorage.__objects)
@@ -33,7 +32,7 @@ class FileStorage:
             json.dump(temp, f)
 
     def reload(self):
-        """Loads storage dictionary from file"""
+        """loaded fileStorage dictionary From file"""
         from models.base_model import BaseModel
         from models.user import User
         from models.place import Place
@@ -57,7 +56,7 @@ class FileStorage:
             pass
 
     def delete(self, obj=None):
-        """ delete obj from __objects if it’s inside """
+        """ remove obj from __objects if it’s inside """
         if obj is not None:
             try:
                 key = '{}.{}'.format(obj.__class__.__name__, obj.id)
